e9c4579171daf23fcdaac4c157b1ce0c69e40938
Matej Liscak <liscakmatej1@gmail.com>
2021-11-27 20:14:14 +0100

    Ispravak veza između tablica, dodani atributi, ispravljen insert

--- portfolio.sql
+++ portfolio.sql
@@ -11,35 +11,35 @@ create table korisnik(
 
 create table novcanik(
     sifra int not null primary key auto_increment,
-    naziv varchar(50),
+    naziv varchar(50) not null,
     korisnik int not null,
     kriptovaluta int not null,
-    vrijednost varchar(50)
+    vrijednost decimal(18,8) not null
 );
 
 create table kriptovaluta(
     sifra int not null primary key auto_increment,
-    naziv varchar(50),
-    oznaka varchar(50),
-    transakcija int not null
+    naziv varchar(50) not null,
+    oznaka varchar(50) not null
 );
 
 create table transakcija(
     sifra int not null primary key auto_increment,
     exchange int not null,
-    kupnja decimal(18.2),
-    prodaja decimal(18.2),
-    oznakakriptovalute varchar(50)
+    kupnja decimal(18,8),
+    prodaja decimal(18,8),
+    datumtransakcije datetime not null,
+    kriptovaluta int not null
 );
 
 create table exchange(
     sifra int not null primary key auto_increment,
-    naziv varchar(50),
+    naziv varchar(50) not null,
     certificiran boolean
 );
 
 alter table novcanik add foreign key (korisnik) references korisnik(sifra);
-alter table kriptovaluta add foreign key (transakcija) references transakcija(sifra);
+alter table transakcija add foreign key (kriptovaluta) references kriptovaluta(sifra);
 alter table novcanik add foreign key (kriptovaluta) references kriptovaluta(sifra);
 alter table transakcija add foreign key (exchange) references exchange(sifra);
 
@@ -53,12 +53,12 @@ insert into exchange(sifra,naziv,certificiran)values
 (null,'Binance',true),
 (null,'Hotbit',false);
 
-insert into transakcija(sifra,exchange,kupnja,prodaja,oznakakriptovalute)values
-(null,1,10040.13,0,'BTC'),
-(null,3,45666.25,0,'ETH'),
-(null,2,0,21342.12,'BNB');
+insert into kriptovaluta(sifra,naziv,oznaka)values
+(null,'Bitcoin','BTC'),
+(null,'Ethereum','ETH'),
+(null,'Binance coin','BNB');
 
-insert into kriptovaluta(sifra,naziv,oznaka,transakcija)values
-(null,'Bitcoin','BTC',1),
-(null,'Ethereum','ETH',2),
-(null,'Binance coin','BNB',3);
+insert into transakcija(sifra,exchange,kupnja,prodaja,datumtransakcije,kriptovaluta)values
+(null,1,'1','0',01-19-2021,2),
+(null,2,'0','3',09-12-2021,1),
+(null,3,'10','0',10-13-2021,3);
